75e46a1e4fb5c06b128355e22e23087d95a85981
Patryk <patryk.janik@t-online.de>
2017-11-06 01:36:47 +0100

    Added MainActivity ohne Funktion, SplashActivity and fire_icon

---
+++ app/src/main/java/de/akbk_horrem/zentralwerkstatt/gereatepruefung_2/SplashActivity.java
@@ -0,0 +1,16 @@
+package de.akbk_horrem.zentralwerkstatt.gereatepruefung_2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+
+    }
+}
